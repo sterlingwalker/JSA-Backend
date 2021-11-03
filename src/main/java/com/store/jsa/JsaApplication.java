e433d44bf65b46509028bfcee4b612f7a98550f5
sterlingwalker <68869426+sterlingwalker@users.noreply.github.com>
2021-11-03 10:34:25 -0400

    Initial Commit

---
+++ src/main/java/com/store/jsa/JsaApplication.java
@@ -0,0 +1,13 @@
+package com.store.jsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/store/jsa/JsaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.store.jsa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
